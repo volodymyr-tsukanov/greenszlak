16501cd1ac4c0434c21dea24150b63a13dc99956
Volodymyr <134626794+volodymyr-tsukanov@users.noreply.github.com>
2025-05-28 18:45:43 +0200

    coredata (db scema setup)

--- greenszlak/greenszlak/Persistence.swift
+++ greenszlak/greenszlak/Persistence.swift
@@ -1,8 +1,8 @@
 //
 //  Persistence.swift
-//  greenszlak
+//  proj1111111
 //
-//  Created by student on 28/05/2025.
+//  Created by student on 07/05/2025.
 //
 
 import CoreData
@@ -21,6 +21,8 @@ struct PersistenceController {
         do {
             try viewContext.save()
         } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             let nsError = error as NSError
             fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
@@ -30,12 +32,23 @@ struct PersistenceController {
     let container: NSPersistentContainer
 
     init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "greenszlak")
+        container = NSPersistentContainer(name: "proj1111111")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+
+                /*
+                 Typical reasons for an error here include:
+                 * The parent directory does not exist, cannot be created, or disallows writing.
+                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                 * The device is out of space.
+                 * The store could not be migrated to the current model version.
+                 Check the error message to determine what the actual problem was.
+                 */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
